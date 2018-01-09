ca87b0b5b692c763b9d6929566b8040ba65a3345
Taylor Lassiter <lassit09@gmail.com>
2018-01-09 23:46:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function sayHiToGrandma(string) {
     return "Are you eating enough?";
   }
 
-  if (string === "I love you, Grandma.") {
+  if (string === `I love you, Grandma.`) {
     return "I love you, too.";
   }
 }
